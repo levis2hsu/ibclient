24fa5d40bdda6b373bf164cc4b4bcc7e22c2bfeb
levis2hsu <levis2hsu@gmail.com>
2016-11-03 16:25:30 -0400

    add encode and decoder messages

--- src/main/java/com/aqitrade/ib/services/messagebus/Encoders.java
+++ src/main/java/com/aqitrade/ib/services/messagebus/Encoders.java
@@ -15,6 +15,27 @@ public class Encoders {
     public static Function<OrderMessage, byte[]> orderMessage = (message) -> {
         return avroMessageEncode(message, OrderMessage.class);
     };
+    
+    public static Function<OrderStatusMessage, byte[]> orderStatusMessage = (message) -> {
+        return avroMessageEncode(message, OrderStatusMessage.class);
+    };
+    
+    public static Function<CashMessage, byte[]> cashMessage = (message) -> {
+        return avroMessageEncode(message, CashMessage.class);
+    };
+    
+    public static Function<InstrumentMessage, byte[]> instrumentMessage = (message) -> {
+        return avroMessageEncode(message, InstrumentMessage.class);
+    };
+    
+    public static Function<PositionMessage, byte[]> positionMessage = (message) -> {
+        return avroMessageEncode(message, PositionMessage.class);
+    };
+    
+    public static Function<TradeMessage, byte[]> tradeMessage = (message) -> {
+        return avroMessageEncode(message, TradeMessage.class);
+    };
+    
 
     private static <T extends GenericContainer> byte[] avroMessageEncode(T message, Class<T> clazz) {
         ByteArrayOutputStream os = new ByteArrayOutputStream();
